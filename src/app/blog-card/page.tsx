c092335bb9bb3323d5f45a01ff6053825fad6991
Charles Goh <15852016+charlesgohck@users.noreply.github.com>
2024-05-26 15:52:38 +0800

    [FEATURE] Blog post edit for focus css for read more button

--- src/app/blog-card/page.tsx
+++ src/app/blog-card/page.tsx
@@ -22,8 +22,9 @@ export default function BlogCard() {
             </div>
             <div className='mt-3'>
               <div className='text-neutral-600 text-base'>Curated vibrants colors for your living, make it pop & calm in the same time.</div>
-              <button className='text-indigo-700 text-base mt-6 hover:text-indigo-800 disabled:text-neutral-600 focus:text-base focus:border-4 focus:border-indigo-100 focus:text-indigo-800'>Read more &#x2192;</button>
-              {/* <button className='rounded-lg border-4 border-indigo-100 text-indigo-800'>Read more &#x2192;</button> */}
+              <button className='text-indigo-700 text-base mt-6 hover:text-indigo-800 disabled:text-neutral-600 focus:text-base focus:border-4 focus:box-content focus:rounded-lg focus:border-indigo-100 focus:text-indigo-800'>Read more &#x2192;</button>
+              <br/>
+              {/* <button className='text-base border-4 box-content rounded-lg border-indigo-100 text-indigo-800'>Read more &#x2192;</button> */}
             </div>
           </div>
         </div>
